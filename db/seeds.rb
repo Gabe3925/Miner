7ae9affeeaabae7bbaaa90903aa0843cda9a8b31
GS888 <gsnydermail@gmail.com>
2014-07-19 19:57:48 -0400

    revised seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@
 
 # CREATE TOOLS:
 pickaxe = Tool.create(name: 'Pick Axe', speed: 1, price: 10)
-shovel = Tool.create(name: 'Shovel', speed: 4, price: 500)
-drill = Tool.create(name: 'Pneumatic Drill', speed: 16, price: 4000)
-bigdrill = Tool.create(name: 'Power Drill', speed: 32, price: 8000)
-borer = Tool.create(name: 'Borer Machine', speed: 100, price: 150000)
+shovel = Tool.create(name: 'Shovel', speed: 4, price: 18000)
+drill = Tool.create(name: 'Pneumatic Drill', speed: 16, price: 80000)
+bigdrill = Tool.create(name: 'Power Drill', speed: 32, price: 205000)
+borer = Tool.create(name: 'Borer Machine', speed: 100, price: 600000)
 
 #CREATE TEST USER
-gabe = User.create(name: 'Gabe', dollars: 500000, :password => "gabegabe", :password_confirmation => "gabegabe", :tool_id => "1")
+gabe = User.create(name: 'Gabe', dollars: 5000000, :password => "gabegabe", :password_confirmation => "gabegabe", :tool_id => "1")
 
 #CREATE TEST-USERS MINE
 gabe.mines << Mine.create(name: 'Gabes Great Mine', depth: 6000)
